7600c2d17c6d384a84a98d9937ef791cf10a22df
Sam Goldenberg <sgolden140@gmail.com>
2022-10-27 12:42:51 -0700

    changed to red pandas

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
   if(message.msg === "image"){
-    fetch('https://some-random-api.ml/img/pikachu')
+    fetch('https://some-random-api.ml/img/red_panda')
           .then(response => response.text())
           .then(data => {
             let dataObj = JSON.parse(data);
